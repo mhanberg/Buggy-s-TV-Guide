67423a784b791cc847e0e0261c4c614b46f8fa30
austin-rauschuber <arauschu@purdue.edu>
2015-03-04 00:43:34 -0500

    Added Actor Popup on Double Click Actor Name

--- src/Popup.java
+++ src/Popup.java
@@ -1,3 +1,5 @@
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -10,9 +12,18 @@ import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
+import thetvdbapi.TheTVDBApi;
 import thetvdbapi.model.*;
+
+import java.util.*;
 
 public class Popup extends JFrame
 {
@@ -76,7 +87,90 @@ public class Popup extends JFrame
 				p.dispatchEvent(new WindowEvent(p, WindowEvent.WINDOW_CLOSING));
 			}
 		});
+		
+		 MouseListener mouseListener = new MouseListener() {
+		      public void mouseClicked(MouseEvent mouseEvent) {
+		        if (mouseEvent.getClickCount() == 2) {
+		          int index = actors.getSelectedIndex();
+		          if (index >= 0) {
+		        	  final TheTVDBApi tvdb = new TheTVDBApi("956FCE4039291BF8");
+		        	  java.util.List<Actor> showactors;
+		        	  try{
+		        		  showactors = tvdb.getActors(show.getId());
+		        		  Popup p = new Popup(showactors.get(index), form);
+		        		  p.setVisible(true);	  
+		        	  } catch (Exception e){
+
+		        	  }
+		        	  
+		          }
+		        }
+		      }
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		actors.addMouseListener(mouseListener);
+
 		addButton.setBounds(335, 7, 89, 23);
 		this.getContentPane().add(addButton);
+	}
+	
+	public Popup(final Actor actor, final MainForm form)
+	{
+		super(actor.getName());
+		final Popup p = this;
+		this.setBounds(250, 250, 600, 400);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.getContentPane().setLayout(null);
+		
+		JLabel name = new JLabel(actor.getName());
+		name.setBounds(10, 11, 200, 14);
+		this.getContentPane().add(name);
+		
+		
+		description = new JTextArea(actor.getRole());
+		description.setBounds(10, 36, 200, 94);
+		description.setColumns(10);
+		description.setEditable(false);
+		description.setLineWrap(true);
+		description.setWrapStyleWord(true);
+		JScrollPane sp = new JScrollPane(description);
+		sp.setBounds(10, 36, 200, 94);;
+		sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		this.getContentPane().add(sp);
+		
+		BufferedImage myPicture;
+		try{
+			myPicture = ImageIO.read(new URL(actor.getImage()));
+			JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+			picLabel.setBounds(220, 11, 350,350);
+			this.getContentPane().add(picLabel);
+		} catch (IOException e){
+			System.out.println("Failed Read");
+		}
+				
+		
 	}
 }
